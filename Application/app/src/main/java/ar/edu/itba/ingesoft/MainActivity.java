8a0c3467f951def3f1a047a28a5e9bedc59e62e5
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2019-08-21 23:20:08 -0300

    Project Creation

---
+++ Application/app/src/main/java/ar/edu/itba/ingesoft/MainActivity.java
@@ -0,0 +1,21 @@
+package ar.edu.itba.ingesoft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ar.edu.itba.ingesoft.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ Application/app/src/main/java/ar/edu/itba/ingesoft/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package ar.edu.itba.ingesoft.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
