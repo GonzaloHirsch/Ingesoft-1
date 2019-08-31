103606ae152f2c02f1b721fff1d1a37d62f4cdee
ig-rib <40872855+ig-rib@users.noreply.github.com>
2019-08-31 11:53:48 -0300

    constraints wip

--- Application/app/src/main/java/ar/edu/itba/ingesoft/ui/login/LoginActivity.java
+++ Application/app/src/main/java/ar/edu/itba/ingesoft/ui/login/LoginActivity.java
@@ -3,11 +3,7 @@ package ar.edu.itba.ingesoft.ui.login;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.TextView;
---
+++ Application/app/src/main/java/ar/edu/itba/ingesoft/ui/login/fragments/BlankFragment.java
@@ -0,0 +1,32 @@
+package ar.edu.itba.ingesoft.ui.login.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import ar.edu.itba.ingesoft.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return null;
+    }
+
+}
