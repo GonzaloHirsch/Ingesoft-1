001e0e510600c79d78c177e1fda40fb7d54d491f
ig-rib <40872855+ig-rib@users.noreply.github.com>
2019-08-31 13:19:11 -0300

    added animations; constraints wip; todo: vuetify

--- Application/app/src/main/java/ar/edu/itba/ingesoft/ui/login/LoginActivity.java
+++ Application/app/src/main/java/ar/edu/itba/ingesoft/ui/login/LoginActivity.java
@@ -1,32 +1,20 @@
 package ar.edu.itba.ingesoft.ui.login;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-import android.widget.TextView;
 
 import ar.edu.itba.ingesoft.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView orCreateNAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        orCreateNAccount = findViewById(R.id.createNewAccountTV);
-        orCreateNAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View x) {
-                Intent intent = new Intent(LoginActivity.this, SignUp.class);
-                startActivity(intent);
-            }
-        });
+
     }
 
 }
--- Application/app/src/main/java/ar/edu/itba/ingesoft/ui/login/fragments/SignUpFragment.java
+++ Application/app/src/main/java/ar/edu/itba/ingesoft/ui/login/fragments/SignUpFragment.java
@@ -3,7 +3,10 @@ package ar.edu.itba.ingesoft.ui.login.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,18 +18,23 @@ import ar.edu.itba.ingesoft.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BlankFragment extends Fragment {
+public class SignUpFragment extends Fragment {
 
-
-    public BlankFragment() {
-        // Required empty public constructor
-    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return null;
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+
+        return view;
     }
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
