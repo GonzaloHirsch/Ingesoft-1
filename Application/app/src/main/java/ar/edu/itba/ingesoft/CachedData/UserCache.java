66e81786f0e2df93f98edcbe8450478eb8a71528
ig-rib <40872855+ig-rib@users.noreply.github.com>
2019-11-29 15:23:34 -0300

    chaching chats

--- Application/app/src/main/java/ar/edu/itba/ingesoft/CachedData/UserCache.java
+++ Application/app/src/main/java/ar/edu/itba/ingesoft/CachedData/UserCache.java
@@ -1,13 +1,48 @@
 package ar.edu.itba.ingesoft.CachedData;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import ar.edu.itba.ingesoft.Classes.Chat;
 import ar.edu.itba.ingesoft.Classes.User;
 
 public class UserCache {
 
     private static User user;
+    private static List<Chat> chats = new ArrayList<>();
 
     public static void SetUser(User userToStore){
         user = userToStore;
+        //todo para los chats
+//        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//        List<Task<DocumentSnapshot>> tasks = new ArrayList<>();
+//        for(String s : user.getChats()) {
+//            tasks.add(db.collection("Chats")
+//                    .document(s)
+//                    .get());
+//
+//        }
+//        Tasks.whenAllSuccess(tasks).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> tasks) {
+//                chats.clear();
+//
+//            }
+//        });
+    }
+
+    public static List<Chat> GetChats(){
+        return chats;
     }
 
     public static User GetUser(){
